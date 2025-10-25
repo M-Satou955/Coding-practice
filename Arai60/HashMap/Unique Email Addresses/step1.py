253c03bc6a45c9012e78a41ade804a0d5d9c1211
M-Satou955 <135571499+M-Satou955@users.noreply.github.com>
2025-10-25 23:47:10 +0900

    add step1 memo

--- Arai60/HashMap/Unique Email Addresses/step1.py
+++ Arai60/HashMap/Unique Email Addresses/step1.py
@@ -1,2 +1,7 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
+        # すでに出たドメイン名 or フルアドレスを格納する
+        prevaddress = {}
+        for email in emails:
+            # 文字列操作を行い+があるかどうかで条件分岐
+        # +がある場合は+〜@の間は無視した文字列を重複しているか調査する対象にする
